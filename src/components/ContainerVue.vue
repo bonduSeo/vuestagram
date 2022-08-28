2fcd4e354729286950138b2a89adf87d889eb0c0
bonduSeo <bondu.seo@gmail.com>
2022-08-29 00:05:33 +0900

    vue 애플코딩강의

---
+++ src/components/ContainerVue.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Post v-for="(post, i) in postData" :key="i" :post="post" />
+  </div>
+</template>
+<script>
+import Post from "./PostVue.vue";
+export default {
+  components: {
+    Post,
+  },
+  data() {
+    return {
+      sampleData: "",
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+  props: {
+    postData: Array,
+  },
+};
+</script>
